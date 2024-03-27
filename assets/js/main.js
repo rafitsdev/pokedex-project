900c52cec34789241bcb1e8fb02a4f9043865553
Rafael Martins <rrmartins2001@gmail.com>
2024-03-27 15:53:13 -0300

    Done - Need some improvement n I'll work on it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,52 @@
-const pokemonList = document.getElementById('pokemonList');
-const loadMoreButton = document.getElementById('loadMoreButton');
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const modal = document.querySelector(".details-modal");
 
 const maxRecords = 151;
 const limit = 10;
 let offset = 0;
 
+function showModal(pokemon) {
+  const modalContent = document.querySelector(".details-modal__content");
 
+  modalContent.innerHTML = `
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.photo}" alt="${pokemon.name}">
+  `;
 
+  modal.classList.add("--active");
+}
+
+function closeModal() {
+  modal.classList.remove("--active");
+}
+
+function attachClickEventToPokemons(pokemons) {
+  pokemons.forEach((pokemon) => {
+    const pokemonElement = document.getElementById(`pokemon-${pokemon.number}`);
+
+    pokemonElement.addEventListener("click", function () {
+      showModal(pokemon);
+    });
+  });
+}
 
 function loadPokemonItems(offset, limit) {
-
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map((pokemon) => `
-      <li class="pokemon ${pokemon.type}">
+    pokemonList.innerHTML += pokemons
+      .map(
+        (pokemon) => `
+      <li class="pokemon ${pokemon.type}" id="pokemon-${
+          pokemon.number
+        }" onclick="showModal(${pokemon.number})">
         <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
           <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            ${pokemon.types
+              .map((type) => `<li class="type ${type}">${type}</li>`)
+              .join("")}
           </ol>
 
           <img src="${pokemon.photo}"
@@ -26,13 +54,17 @@ function loadPokemonItems(offset, limit) {
 
         </div>
       </li>
-    `).join('');
-  })
+    `
+      )
+      .join("");
+
+    attachClickEventToPokemons(pokemons);
+  });
 }
 
 loadPokemonItems(offset, limit);
 
-loadMoreButton.addEventListener('click', () => {
+loadMoreButton.addEventListener("click", () => {
   offset += limit;
   const qtdRecordWithNextPage = offset + limit;
 
@@ -44,4 +76,4 @@ loadMoreButton.addEventListener('click', () => {
   } else {
     loadPokemonItems(offset, limit);
   }
-})
+});
